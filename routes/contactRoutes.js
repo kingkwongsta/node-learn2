07b50dc2de1c8cb847d9f6853b60f4a003fc2106
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-04-17 20:03:38 -0700

    create get put delete routes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,7 +2,23 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/").get((req, res) => {
-  res.status(200).json({ message: "testing testing" });
+  res.status(200).json({ message: "Getting contact" });
+});
+
+router.route("/:id").put((req, res) => {
+  res.status(200).json({ message: `Get contact - ${req.params.id}` });
+});
+
+router.route("/").post((req, res) => {
+  res.status(200).json({ message: "Creating contact" });
+});
+
+router.route("/:id").put((req, res) => {
+  res.status(200).json({ message: `Update contact - ${req.params.id}` });
+});
+
+router.route("/:id").delete((req, res) => {
+  res.status(200).json({ message: `Delete contact - ${req.params.id}` });
 });
 
 module.exports = router;
