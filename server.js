64c43706f210ee544cfd7af6b9879b31b17b1dfd
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-04-17 19:53:36 -0700

    initial config

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("hello world tesst");
+const express = require("express");
+// const dotenv = require("dotenv").config();
+
+const app = express();
+
+const port = 5000;
+// const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("hello");
+});
+
+app.get("/api/contacts", (req, res) => {
+  res.send("testing testing");
+});
